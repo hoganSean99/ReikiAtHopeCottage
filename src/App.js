261564baae3fc3074de9e2d68b606ba4450af2ea
Sean Hogan <32335404+hoganSean99@users.noreply.github.com>
2024-06-26 01:54:28 +0100

    Created Nav Bar Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
 import './App.css';
-import home from 'pages/home.js'
+import Home from './pages/Home.js'
 
 function App() {
   return (
-    <div className="App">
-      <home />
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        {/* You can add more routes here */}
+        {/* Example:
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+        <Route path="/contact" component={Contact} />
+        */}
+      </Switch>
+    </Router>
   );
 }
 
